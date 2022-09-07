8b1c5f527640fc0111690877d091c26364918a36
Sean Sundberg <seansund@us.ibm.com>
2022-09-07 13:15:34 -0500

    Converts to use gitops provider
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage0.tf
+++ test/stages/stage0.tf
@@ -6,12 +6,15 @@ terraform {
   }
 }
 
-provider "gitops" {
-  username = var.git_username
-  token = var.git_token
-  bin_dir  = module.setup_clis.bin_dir
+module setup_test_clis {
+  source = "github.com/cloud-native-toolkit/terraform-util-clis.git"
+
+  bin_dir = "${path.cwd}/test_bin_dir"
+  clis = ["kubectl", "oc"]
 }
 
-module setup_clis {
-  source = "github.com/cloud-native-toolkit/terraform-util-clis.git"
+resource local_file bin_dir {
+  filename = "${path.cwd}/.bin_dir"
+
+  content = module.setup_test_clis.bin_dir
 }
